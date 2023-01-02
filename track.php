1b7b070c1e5daaef9e01230648315760bf2e1355
Hiroki Yanase <60053865+YanaseHiroki@users.noreply.github.com>
2023-01-02 21:00:55 +0900

    ブレイクしたところの各変数を表示する機能の実装

--- track.php
+++ track.php
@@ -2,9 +2,8 @@
 require './func/element.php';
 require './func/dump.php';
 require './func/check.php';
+require './func/process.php';
 require './module/header.php';
-
-$vars_initial = get_defined_vars();
 
 // ファイルの保存先
 $path = './uploaded/userfile.php';
@@ -23,27 +22,40 @@ if( !empty($_FILES['userfile']['tmp_name']) && is_uploaded_file($_FILES['userfil
 // 保存したファイルの読み込み
 $contents = file($path, FILE_IGNORE_NEW_LINES);
 
-// タグの置換
-// $contents = h($contents);
+// 読み込んだコードからhtmlを削除
+$contents_str = implode("NeXtLiNe", $contents);
+preg_match_all('/\?php.+?\?>/', $contents_str, $contents_arr);
+$contents_arr = $contents_arr[0];
+for($i=0; $i<count($contents_arr); $i++) {
+    $contents_arr[$i] = str_replace('?php', '', $contents_arr[$i]);
+    $contents_arr[$i] = str_replace('?>', '', $contents_arr[$i]);
+}
+$contents_str = implode("NeXtLiNe", $contents_arr);
+$contents_arr = explode("NeXtLiNe", $contents_str);
+$contents = array_filter($contents_arr);
 
-
-// 各行にget_defined_vars()を埋め込む
+// $contentsの各行の間にget_defined_vars()関数を埋め込む
+$i = 0;
 foreach ($contents as $line) {
     $inserted[] = $line;
-    $inserted[] = '<?php $vars_in_the_way[] = array_diff(get_defined_vars(),$vars_initial); ?>';
+    $inserted[] = '$vars_in_the_way[' . $i . '] = array_diff(get_defined_vars(),$vars_initial);';
+    $i++;
 }
-array_unshift($inserted, '<?php $vars_initial = get_defined_vars(); ?>');
+array_unshift($inserted, '<?php $vars_initial = get_defined_vars();');
 
-var_export($inserted);
-// $inserted = h($inserted);
-// p('inserted');
+// 内容確認
+// $h_inserted = h($inserted);
+// p('h_inserted');
 
 // 配列$insertedを結合してファイル保存
 $inserted_str = implode("\n", $inserted);
 file_put_contents('./uploaded/inserted.php', $inserted_str);
 
 // ファイルinserted.phpを呼び出して裏で実行
-
+echo '<div hidden>ここは表示されない';
+require './uploaded/inserted.php';
+echo '</div>';
+e('vars_in_the_way');
 echo '<pre>';
 $i = 1;
 
@@ -62,8 +74,8 @@ foreach ($contents as $line) {
     $line = htmlentities($line);
     echo $line, "\n";
     if ($_REQUEST['line'] == $i) {
-    $vars = array_diff(get_defined_vars(),$vars_initial);
-    echo "</pre><pre>", var_export($vars), "</pre><pre>";
+        $vars = $vars_in_the_way[$i-1];
+        echo "</pre><pre>", var_export($vars), "</pre><pre>";
     }
     $i++;
 
